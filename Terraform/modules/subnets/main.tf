28a10e410ef66ef9e279ec2193b328671bdf87d7
Amr Hatem <108996785+moraa121212@users.noreply.github.com>
2025-06-08 04:11:42 +0300

    Update main.tf

--- Terraform/modules/subnets/main.tf
+++ Terraform/modules/subnets/main.tf
@@ -1,5 +1,11 @@
-resource "aws_subnet" "project_subnet" {
-  for_each = zipmap(var.availability_zones, var.subnet_cidrs)
+resource "aws_subnet" "project_subnet-private" {
+  for_each = zipmap(var.availability_zones, var.subnet_cidrs-prv)
+  vpc_id            = var.vpc_id
+  cidr_block        = each.value
+  availability_zone = each.key
+}
+resource "aws_subnet" "project_subnet-puplic" {
+  for_each = zipmap(var.availability_zones, var.subnet_cidrs-pup)
   vpc_id            = var.vpc_id
   cidr_block        = each.value
   availability_zone = each.key
