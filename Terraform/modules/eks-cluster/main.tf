fd0b314a113bbbfa6981650eea50b9982f69a557
Amr Hatem <108996785+moraa121212@users.noreply.github.com>
2025-06-08 08:19:52 +0300

    Update main.tf

--- Terraform/modules/eks-cluster/main.tf
+++ Terraform/modules/eks-cluster/main.tf
@@ -1,8 +1,6 @@
 resource "aws_eks_cluster" "project-cluster" {
   name     = var.cluster_name
   role_arn = var.cluster_role_arn
-  version  = var.k8s_version
-
   vpc_config {
     subnet_ids = var.subnet_ids
   }
