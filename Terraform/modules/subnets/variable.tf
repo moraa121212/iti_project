7f4a4cf083dafe8cefea2648231590a9c0505bf2
Amr Hatem <108996785+moraa121212@users.noreply.github.com>
2025-06-08 04:19:21 +0300

    Update variable.tf

--- Terraform/modules/subnets/variable.tf
+++ Terraform/modules/subnets/variable.tf
@@ -2,11 +2,11 @@ variable "vpc_id" {
   description = "The VPC ID where subnets will be created"
   type        = string
 }
-variable "subnet_cidrs-prv" {
+variable "subnet_cidrs_prv" {
   type        = list(string)
   description = "List of subnet CIDRs"
 }
-variable "subnet_cidrs-pup" {
+variable "subnet_cidrs_pup" {
   type        = list(string)
   description = "List of subnet CIDRs"
 }
