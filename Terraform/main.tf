743c7f4ac86eae4195608bb919fbd891ce9ac61a
Amr Hatem <108996785+moraa121212@users.noreply.github.com>
2025-06-08 09:04:50 +0300

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -6,16 +6,15 @@ module "vpc" {
 
 module "subnets" {
   source = "./modules/subnets"
-  vpc_id             = module.vpc.vpc_id
-  subnet_cidrs       = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  vpc_id = module.vpc.vpc_id
+  subnet_cidrs_prv = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  subnet_cidrs_pup = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 }
-
-
 module "internet_gateway" {
   source = "./modules/internet_gateway"
   vpc_id = module.vpc.vpc_id
-  public_subnet_ids = module.subnets.subnet_ids
+  public_subnet_ids = module.subnets.subnet_ids_pup
 }
 
 module "SG" {
@@ -38,8 +37,10 @@ module "eks-cluster" {
   source = "./modules/eks-cluster"
   cluster_name = "project"
   cluster_role_arn = module.IAM-Role.role_arn
-  #version = 1.31
-  subnet_ids = module.subnets.subnet_ids
+  subnet_ids = concat(
+    module.subnets.subnet_ids_prv, 
+    module.subnets.subnet_ids_pup
+  )
   cluster_role_policy_attachment_depends_on = [
     module.IAM-Role
   ]
@@ -50,7 +51,7 @@ module "nodes-group" {
   cluster_name = module.eks-cluster.cluster_name
   node_group_name = "project-nodes"
   node_role_arn = module.node-role.role_arn
-  subnet_ids = module.subnets.subnet_ids
+  subnet_ids = module.subnets.subnet_ids_prv
   desired_size    = 2
   max_size        = 3
   min_size        = 1
