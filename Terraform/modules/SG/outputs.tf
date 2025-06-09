74cb0df84dda6e7136da5a5c9012e5d699298f56
Amr Hatem <108996785+moraa121212@users.noreply.github.com>
2025-06-09 07:38:27 +0300

    Delete Terraform/modules/SG directory

--- Terraform/modules/SG/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "worker_sg_id" {
-  description = "ID of the worker node security group"
-  value       = aws_security_group.eks_worker_sg.id
-}
--- Terraform/modules/SG/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "vpc_id" {
-  description = "VPC ID where the security group will be created"
-  type        = string
-}
-
-variable "control_plane_cidr_blocks" {
-  description = "List of CIDR blocks allowed to access control plane"
-  type        = list(string)
-}
