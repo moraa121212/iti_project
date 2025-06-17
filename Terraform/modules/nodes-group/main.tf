b55b97bcfde022ae168d1b8b01d7ede0b486ad2f
Amr Hatem <108996785+moraa121212@users.noreply.github.com>
2025-06-17 18:27:49 +0300

    Update main.tf

--- Terraform/modules/nodes-group/main.tf
+++ Terraform/modules/nodes-group/main.tf
@@ -3,9 +3,7 @@ resource "aws_eks_node_group" "project-nodes" {
   node_group_name = var.node_group_name
   node_role_arn   = var.node_role_arn
   subnet_ids      = var.subnet_ids_prv
- security_group_ids = [
-    module.SG.worker_sg_id      
-  ]
+ security_group_ids = [ module.SG.worker_sg_id ]
   scaling_config {
     desired_size = var.desired_size
     max_size     = var.max_size
